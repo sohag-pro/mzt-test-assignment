5ff52a7e98f707a6dec99056cd22412a6a9f001d
Sohag Hasan <shohaghasan575@gmail.com>
2022-04-29 11:30:31 +0600

    add | csrf and format page

--- resources/views/candidates/index.blade.php
+++ resources/views/candidates/index.blade.php
@@ -1,29 +1,34 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-      <meta charset="utf-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-     <title>MZT test assignment</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
- <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap" rel="stylesheet">
+    <title>MZT test assignment</title>
 
-      <style>
-       body {
-    font-family: 'Roboto', sans-serif;
-  }
-   </style>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap" rel="stylesheet">
 
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
-    <body>
+    <style>
+        body {
+            font-family: 'Roboto', sans-serif;
+        }
+
+    </style>
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
     <div id="app">
- <div class="w-full p-6 bg-teal-100 text-right font-bold">Your wallet has: {{$coins ?? '?' }} coins</div>
-    <candidates :candidates="{{ json_encode($candidates) }}">
-</candidates>
+        <div class="w-full p-6 bg-teal-100 text-right font-bold">Your wallet has: {{ $coins ?? '?' }} coins</div>
+        <candidates :candidates="{{ json_encode($candidates) }}">
+        </candidates>
     </div>
 
     <script src="{{ mix('/js/app.js') }}"></script>
-    </body>
+</body>
+
 </html>
